17f8f0c7b656f0f79f733df5da00ca89e127062b
Garrett Battaglia <Garrett.Battaglia@dialexa.com>
2022-12-02 15:22:31 +0000

    Day 2 Part 2

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -15,21 +15,22 @@ fn main() {
     }
 }
 
-fn solve_problem(data: String){
+fn solve_problem(data: String) {
     let part1 = data
         .trim()
         .split("\n")
         .map(|x| {
-            let hand = x.trim().split(" ", ).collect::<Vec<&str>>();
+            let hand = x.trim().split(" ").collect::<Vec<&str>>();
             let shape_pts;
-            match hand[1]{
+            match hand[1] {
                 "X" => shape_pts = 1, //rock
                 "Y" => shape_pts = 2, //paper
                 "Z" => shape_pts = 3, //scissors
-                _ => panic!("bad char {} in player's hand", hand[1])
+                _ => panic!("bad char {} in player's hand", hand[1]),
             }
             let mut win_pts = 0;
-            if hand[0] == "A" { //rock
+            if hand[0] == "A" {
+                //rock
                 if hand[1] == "Y" {
                     win_pts = 6;
                 } else if hand[1] == "Z" {
@@ -37,34 +38,105 @@ fn solve_problem(data: String){
                 } else if hand[1] == "X" {
                     win_pts = 3;
                 }
-            } else if hand[0] == "B" { //paper
+            } else if hand[0] == "B" {
+                //paper
                 if hand[1] == "X" {
                     win_pts = 0;
                 } else if hand[1] == "Z" {
                     win_pts = 6;
-                }else if hand[1] == "Y" {
+                } else if hand[1] == "Y" {
                     win_pts = 3;
                 }
-            } else if hand[0] == "C" { //scissors
+            } else if hand[0] == "C" {
+                //scissors
                 if hand[1] == "X" {
                     win_pts = 6;
                 } else if hand[1] == "Y" {
                     win_pts = 0;
-                }else if hand[1] == "Z" {
+                } else if hand[1] == "Z" {
                     win_pts = 3;
                 }
             } else {
                 panic!("bad char {} in openent's hand", hand[1])
             }
             shape_pts + win_pts
-        }).sum::<i64>();
+        })
+        .sum::<i64>();
+
+    let part2 = data
+        .trim()
+        .split("\n")
+        .map(|x| {
+            let scissors_pts = 3;
+            let paper_pts = 2;
+            let rock_pts = 1;
+
+            let hand = x.trim().split(" ").collect::<Vec<&str>>();
+
+            let shape_pts;
+            let win_pts;
+
+            if hand[1] == "X" {
+                //lose
+                win_pts = 0;
+                if hand[0] == "A" {
+                    //rock
+                    shape_pts = scissors_pts;
+                } else if hand[0] == "B" {
+                    //paper
+                    shape_pts = rock_pts;
+                } else if hand[0] == "C" {
+                    //scissors
+                    shape_pts = paper_pts;
+                } else {
+                    panic!("bad char {} in openent's hand", hand[0])
+                }
+            } else if hand[1] == "Y" {
+                //draw
+                win_pts = 3;
+                if hand[0] == "A" {
+                    //rock
+                    shape_pts = rock_pts;
+                } else if hand[0] == "B" {
+                    //paper
+                    shape_pts = paper_pts;
+                } else if hand[0] == "C" {
+                    //scissors
+                    shape_pts = scissors_pts;
+                } else {
+                    panic!("bad char {} in openent's hand", hand[0])
+                }
+            } else if hand[1] == "Z" {
+                //win
+                win_pts = 6;
+                if hand[0] == "A" {
+                    //rock
+                    shape_pts = paper_pts;
+                } else if hand[0] == "B" {
+                    //paper
+                    shape_pts = scissors_pts;
+                } else if hand[0] == "C" {
+                    //scissors
+                    shape_pts = rock_pts;
+                } else {
+                    panic!("bad char {} in openent's hand", hand[0])
+                }
+            } else {
+                panic!("bad char {} in player's hand", hand[1])
+            }
+
+            shape_pts + win_pts
+        })
+        .sum::<i64>();
 
     println!("solution pt1: {}", part1);
-    println!("solution pt2: {}", "???");
+    println!("solution pt2: {}", part2);
 }
 
 fn read_file<P>(filename: P) -> io::Result<String>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let mut file = File::open(filename)?;
     let mut contents = String::new();
 
